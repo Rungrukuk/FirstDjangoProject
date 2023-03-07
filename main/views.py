7ba373c4e17c2fdf90070362db9af16e7b2a020c
Rungrukuk <Rungrukuk@gmail.com>
2023-03-07 19:08:04 +0400

    First Lesson has overed

--- main/views.py
+++ main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import ToDoList, Item
 
-
-def index(response):
-    return HttpResponse("<h1>tech with kamal</h1>")
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id =1)
+    return HttpResponse("<h1>%s</h1> <br></br> <p>%s</p>" %(ls.id, str(item.text)))
 # Create your views here.
 
-
-def v1(response):
-    return HttpResponse("<h1>teze sayt?</h1>")
