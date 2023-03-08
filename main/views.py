9b8eb9a8018de0c5869065f49c1956db41590e13
Rungrukuk <Rungrukuk@gmail.com>
2023-03-08 15:44:02 +0400

    First Django Projects Lesson 1

--- main/views.py
+++ main/views.py
@@ -6,5 +6,7 @@ def index(response, name):
     ls = ToDoList.objects.get(name=name)
     item = ls.item_set.get(id =1)
     return HttpResponse("<h1>%s</h1> <br></br> <p>%s</p>" %(ls.id, str(item.text)))
+
+
 # Create your views here.
 
