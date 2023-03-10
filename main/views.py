a47f460513e5be6437c39342afd09bd1bc3a10b8
Rungrukuk <Rungrukuk@gmail.com>
2023-03-10 13:02:06 +0400

    Last Lesson Has Finished

--- main/views.py
+++ main/views.py
@@ -6,41 +6,44 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+    if ls in response.user.todolist.all():
 
-    return render(response, "main/base.html", {"ls": ls})
+        if response.method == "POST":
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c"+str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
+                else:
+                    print("invalid")
+        return render(response, "main/list.html", {"ls": ls})
+    return render(response, "main/404.html", {})
 
 
 def home(response):
     return render(response, "main/home.html", {})
 
 
-def list(response, id):
-    ls = ToDoList.objects.get(id=id)
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c"+str(item.id)) == "clicked":
-                    item.complete = True
-                else:
-                    item.complete = False
-                item.save()
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-    return render(response, "main/list.html", {"ls": ls})
+def list(response):
+    return render(response, "main/view.html", {})
 
 
 def create(response):
     if response.method == "POST":
         form = CreateNewList(response.POST)
         if form.is_valid():
-            t = ToDoList(name=form.cleaned_data["name"])
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)
         return HttpResponseRedirect("/%i" % t.id)
     else:
         form = CreateNewList()
